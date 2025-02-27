00f7fdbbe64fad65aa297792f3275b0f1bd0cd5a
Keong96 <chinkeongtan96@gmail.com>
2025-02-27 13:02:59 +0800

    a

--- index.js
+++ index.js
@@ -310,7 +310,7 @@ app.post('/make-call', async (req, res) => {
 app.post('/voice-response', (req, res) => {
   const conferenceName = req.query.conference;
   const twiml = new twilio.twiml.VoiceResponse();
-  console.log("voice ->"+conferenceName)
+  console.log("voice-response ->"+conferenceName)
 
   // When the recipient answers, they are joined to the conference.
   twiml.dial().conference(conferenceName, {
